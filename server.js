41e4e35f75558845c306b67fd5c41775318ee569
Sophia <sophkiani@gmail.com>
2021-11-12 17:44:26 -0600

    updated everything

--- server.js
+++ server.js
@@ -15,16 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds231725.mlab.com:31725/heroku_sqc6mlww", {});
-
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://mirana85:<password>@sophk.nes68.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {});
 
 // routes
 app.use(require("./routes/api.js"));
