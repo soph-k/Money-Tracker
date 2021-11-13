7f2233394bdc4c00f19a2dd823bdfb00a9c958d8
Sophia <sophkiani@gmail.com>
2021-11-12 18:06:35 -0600

    updated server.js

--- server.js
+++ server.js
@@ -23,18 +23,3 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
-
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {});
-
-// routes
-app.use(require("./routes/api.js"));
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
